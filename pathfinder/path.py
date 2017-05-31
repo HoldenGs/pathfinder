1f8af18962f43e9f15d38fba449191153500ef34
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-05-31 20:15:42 +0000

    add path.py

--- pathfinder/path.py
+++ pathfinder/path.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 
+"""
+Application for finding quickest path between two Wikipedia pages.
 
-from flask import Flask
+"""
+from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
 
+@app.route('/', methods = ['POST'])
+def pathfinder():
+    """Handle form data from client."""
+    search_items = request.form['search'].split(",")
+    start_item = search_items[0]
+    end_item = search_items[1]
+    return (start_item, end_item)
+
 @app.route('/')
-def pathfinder_index():
-    
+def index():
+    """Render webpage."""
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
